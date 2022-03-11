8389ced7130cfb262e80dde24bd78b1e47b0beaa
Sta2yan <s2_stas@mail.ru>
2022-03-11 18:54:49 +0300

    Добавьте файлы проекта.

---
+++ MergingIntoOneCollection/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MergingIntoOneCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] textArray1 = { "Привет", "Stas", "Мир", "C#", "Stas", "Разработчик игр" };
+            string[] textArray2 = { "Hello", "Стас", "Мир", "C#", "Программист", "Игры", "C#", "Игры" };
+            List<string> textsList = new List<string>();
+
+            for (int i = 0; i < textArray1.Length; i++)
+            {
+                if (textsList.Contains(textArray1[i]) == false)
+                {
+                    textsList.Add(textArray1[i]);
+                }
+            }
+
+            for (int i = 0; i < textArray2.Length; i++)
+            {
+                if (textsList.Contains(textArray2[i]) == false)
+                {
+                    textsList.Add(textArray2[i]);
+                }
+            }
+
+            foreach (string text in textsList)
+            {
+                Console.WriteLine(text);
+            }
+        }
+    }
+}
